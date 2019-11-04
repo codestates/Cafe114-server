14eb2994199de0dc646fb01d713a52b408d4fd92
Hae Jun Kim <dan.kimhaejun@gmail.com>
2019-11-04 15:27:52 +0900

    add cors2

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ console.log('s3 connect region : ', s3.config.region);
 
 app.use(cors());
 app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   res.header('Access-Control-Allow-Credentials', true);
