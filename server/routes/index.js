fc528bd58b4ae057b82b28087c88914794d24a75
Hae Jun Kim <dan.kimhaejun@gmail.com>
2019-11-08 14:33:44 +0900

    Merge to master

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,9 @@
-const controller = require('../controllers/index');
-const checkToken = require('../middlewares/auth').checkToken;
-const router = require('express').Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  console.log('__dirname', __dirname);
-  res.sendFile(__dirname + '/index.html');
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
-
-router.get('/signout', controller.get.signOut);
-
-router.post('/signup', controller.post.signUp);
-
-router.post('/signup/email', controller.post.checkEmail);
-
-router.post('/signin', controller.post.signIn);
-
-router.use('/signin/kakao', checkToken);
-
-router.post('/signin/kakao', controller.post.kakao);
 
 module.exports = router;
